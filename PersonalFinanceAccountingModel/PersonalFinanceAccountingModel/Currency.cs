79d6851c44ec9409822fb6cc2b393675cf9675ee
Galina Rubo <Rubo_eh74@itstepru.onmicrosoft.com>
2023-01-26 13:22:09 +0700

    Добавьте файлы проекта.

---
+++ PersonalFinanceAccountingModel/PersonalFinanceAccountingModel/Currency.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalFinanceAccountingModel.Library
+{
+    /// <summary>
+    /// Класс Валюта
+    /// </summary>
+    public class Currency
+    {
+        /// <summary>
+        ///  Id - идентификатор валюты
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Name - название валюты
+        /// </summary>
+        public string? Name { get; set; }
+
+        /// <summary>
+        ///  Code - код валюты
+        /// </summary>
+        public int Code { get; set; }
+    }
+}
---
+++ PersonalFinanceAccountingModel/PersonalFinanceAccountingModel/LogOperation.cs
@@ -0,0 +1,33 @@
+﻿namespace PersonalFinanceAccountingModel.Library
+{
+    /// <summary>
+    /// класс Запись операции
+    /// </summary>
+    public class LogOperation
+    {
+        /// <summary>
+        /// идентификатор записи
+        /// </summary>
+        public int Id { get; set; }
+
+        /// <summary>
+        /// идентификатор кошелька
+        /// </summary>
+        public int IdWallet { get; set; }
+
+        /// <summary>
+        /// время проведения транзакции
+        /// </summary>
+        public DateTime? DateTime { get; set; }
+
+        /// <summary>
+        /// идентификатор операции
+        /// </summary>
+        public string? IdOperation { get; set; }
+
+        /// <summary>
+        /// сумма транзакции
+        /// </summary>
+        public double Sum { get; set; }
+    }
+}
